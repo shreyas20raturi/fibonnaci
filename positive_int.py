7582bd02af2474381b08df3357f949785a9851bd
shreyas20raturi <77580401+shreyas20raturi@users.noreply.github.com>
2022-03-10 23:19:30 +0530

    Create positive_int.py

---
+++ positive_int.py
@@ -0,0 +1,15 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+output=[]
+output2=[]
+for num in list1:
+    if num>0:
+     output.append(num)
+print("Input: list1=",list1)
+print("Output: ",output)
+
+for x in list2:
+  if x>0:
+    output2.append(x)
+print("Input: list2=",list2)
+print("Output: ",output2)
